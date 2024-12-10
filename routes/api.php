6978a4ad062a0ed027a0fa4cd36ee8434d2d5f68
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-12-10 18:20:54 +0700

    change(route): adding prefix

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\TechnicianController;
-use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\UserAttachmentController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\OrderAttachmentController;
-use App\Http\Controllers\ReviewController;
-use App\Http\Controllers\ProfileController; 
-use App\Http\Controllers\OrderManagementController; 
-use App\Http\Controllers\AttachmentController;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\{
+    AuthController,
+    UserController,
+    TechnicianController,
+    CustomerController,
+    UserAttachmentController,
+    OrderController,
+    OrderManagementController,
+    ReviewController,
+    ProfileController,
+    AttachmentController
+};
 
-Route::middleware(['api'])->group(function () {
+Route::prefix('api/v1')->middleware(['api'])->group(function () {
     Route::get('/', function () {
         return response()->json([
             'message' => 'Welcome to OnlyFix BE',
@@ -27,41 +28,44 @@ Route::middleware(['api'])->group(function () {
         ]);
     });
 
-    // Auth routes
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
-    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth');
-    Route::get('current-user', [AuthController::class, 'currentUser'])->middleware('auth');
-    Route::get('/csrf-token', function () {
-        return response()->json([
-            'csrfToken' => csrf_token(),
-        ]);
+    // Auth Routes
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('register', 'register');
+        Route::post('login', 'login');
+        Route::post('logout', 'logout')->middleware('auth:api');
+        Route::get('current-user', 'currentUser')->middleware('auth:api');
+        Route::get('csrf-token', function () {
+            return response()->json(['csrfToken' => csrf_token()]);
+        });
     });
-    
-    // User routes
-    Route::resource('users', UserController::class);
-    Route::resource('technicians', TechnicianController::class);
-    Route::resource('customers', CustomerController::class);
-    Route::get('profile/{user_id}', [ProfileController::class, 'show']); 
-    Route::put('profile/{user_id}', [ProfileController::class, 'update']);
-    Route::resource('user_attachments', UserAttachmentController::class);
 
-    // Order routes
-    Route::resource('orders', OrderController::class);
-    Route::get('order/{order_id}', [OrderManagementController::class, 'show']);  
-    Route::post('orders', [OrderManagementController::class, 'store']); 
-    Route::post('orders/{order_id}/attachments', [OrderManagementController::class, 'addAttachment']); 
-    Route::post('orders/{order_id}/review', [OrderManagementController::class, 'addReview']); 
-    Route::resource('order_attachments', OrderAttachmentController::class);
+    // User Routes
+    Route::resource('users', UserController::class)->except(['create', 'edit']);
+    Route::resource('technicians', TechnicianController::class)->only(['index', 'show', 'store', 'update', 'destroy']);
+    Route::resource('customers', CustomerController::class)->only(['index', 'show', 'store', 'update', 'destroy']);
+    Route::controller(ProfileController::class)->group(function () {
+        Route::get('profile/{user_id}', 'show');
+        Route::put('profile/{user_id}', 'update');
+    });
+    Route::resource('user-attachments', UserAttachmentController::class)->except(['create', 'edit']);
 
-    // Review routes
-    Route::resource('reviews', ReviewController::class);
-    
-    // Attachment
-    Route::get('attachments/user/{user_id}', [AttachmentController::class, 'getUserAttachments']); 
-    Route::get('attachments/order/{order_id}', [AttachmentController::class, 'getOrderAttachments']);  
-    Route::post('attachments/user/{user_id}', [AttachmentController::class, 'storeUserAttachment']);  
-    Route::post('attachments/order/{order_id}', [AttachmentController::class, 'storeOrderAttachment']); 
-}
-);
+    // Order Routes
+    Route::resource('orders', OrderController::class)->except(['create', 'edit']);
+    Route::controller(OrderManagementController::class)->group(function () {
+        Route::get('orders/{order_id}', 'show');
+        Route::post('orders', 'store');
+        Route::post('orders/{order_id}/attachments', 'addAttachment');
+        Route::post('orders/{order_id}/review', 'addReview');
+    });
 
+    // Review Routes
+    Route::resource('reviews', ReviewController::class)->only(['index', 'show', 'store', 'update', 'destroy']);
+
+    // Attachment Routes
+    Route::controller(AttachmentController::class)->group(function () {
+        Route::get('attachments/user/{user_id}', 'getUserAttachments');
+        Route::get('attachments/order/{order_id}', 'getOrderAttachments');
+        Route::post('attachments/user/{user_id}', 'storeUserAttachment');
+        Route::post('attachments/order/{order_id}', 'storeOrderAttachment');
+    });
+});
