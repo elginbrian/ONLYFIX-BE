f48c1b000c822ace00c8b24e7100a6ec2d4658a3
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-12-16 23:39:45 +0700

    change(cors): update allowed origin

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'supports_credentials' => true,
-    'allowed_origins' => ['http://localhost:3000', 'https://onlyfix.up.railway.app'],  
+    'allowed_origins' => ['http://localhost:3000', 'https://onlyfix.up.railway.app', 'https://onlyfix.elginbrian.com'],  
     'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization', 'Accept'],
     'allowed_methods' => ['*'],  
     'exposed_headers' => [],
